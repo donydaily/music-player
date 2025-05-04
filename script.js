af0ed6c96ea7c6f0ab539c4653ff81d8744ecc36
donydaily <fajarromdoni1@gmail.com>
2025-05-04 10:41:56 +0700

    Update code...

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
     { title: 'Kehadiranmu', artist: 'Vagetoz', art: 'images/kehadiranmu.png', src: 'audio/kehadiranmu.mp3' },
     { title: 'Main Hati', artist: 'Andra And The BackBone', art: 'images/main_hati.png', src: 'audio/main_hati.mp3' },
     { title: 'Masih Ada', artist: 'Ello', art: 'images/masih_ada.png', src: 'audio/masih_ada.mp3' }
+    { title: 'Supernatural', artist: 'Ariana Grande', art: 'images/supernatural.png', src: 'audio/supernatural.mp3' }
   ];
 
   const canvas = document.getElementById('visualizer');
